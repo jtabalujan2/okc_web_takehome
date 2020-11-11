6f96bff06b742e9badad7ec473f44fb32e070206
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 00:26:28 -0800

    Created main structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-require('./App.scss');
+import AboutMeFormList from './AboutMeFormList';
+import AboutMeEssay from './AboutMeEssay';
 
-const App = ({ counter, increment }) => (
-  <div className="match-area">
-    Counter (to make sure redux works): {counter}
-    <br />
-    <br />
-    <button onClick={increment}>
-      Increment
-    </button>
+const App = ({ fieldOrder, fieldAnswers }) => (
+  <div className="flex justify-center items-center min-h-screen">
+    <div className="match-area flex">
+      <AboutMeFormList fieldOrder={fieldOrder} />
+      <AboutMeEssay fieldAnswers={fieldAnswers} />
+    </div>
   </div>
 );
 
 App.propTypes = {
-  counter: PropTypes.number.isRequired,
-  increment: PropTypes.func.isRequired,
+  fieldOrder: PropTypes.arrayOf(PropTypes.string).isRequired,
+  fieldAnswers: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default App;
