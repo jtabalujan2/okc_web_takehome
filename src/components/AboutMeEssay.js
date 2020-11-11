5778c9950a659a6cbdb842ae4ff4340f5e62fbd7
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 13:45:45 -0800

    Added edit button to appear only after all fields are done

--- src/components/AboutMeEssay.js
+++ src/components/AboutMeEssay.js
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-const AboutMeEssay = ({ fieldAnswers }) => (
+const AboutMeEssay = ({ fieldAnswers, isAllFieldsAnswered }) => {
+  return (
   <div className="essay">
     <h2>Your essay text</h2>
     <pre>{JSON.stringify(fieldAnswers)}</pre>
+    <pre>{isAllFieldsAnswered}</pre>
+    {isAllFieldsAnswered && 
+      <button>Edit</button>
+    }
   </div>
-);
+)};
 
 AboutMeEssay.propTypes = {
   fieldAnswers: PropTypes.objectOf(PropTypes.string).isRequired,
+  isAllFieldsAnswered: PropTypes.bool.isRequired
 };
 
 export default AboutMeEssay;
