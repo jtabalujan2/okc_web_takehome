b66fcbb87cb7e7c134883e0e1c66008f2245b015
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 15:35:13 -0800

    Removed unused prop

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import AboutMeEssay from '../containers/AboutMeEssay';
 
 const okcLogo = 'https://cdn.okccdn.com/media/img/hub/mediakit/okcupid_darkbg_2019.png';
 
-const App = ({ fieldOrder, fieldAnswers }) => (
+const App = ({ fieldOrder }) => (
   <div className="flex flex-col justify-center items-center min-h-screen">
     <img src={okcLogo} alt="OkCupid Logo" />
     <div className="match-area flex">
@@ -18,7 +18,6 @@ const App = ({ fieldOrder, fieldAnswers }) => (
 
 App.propTypes = {
   fieldOrder: PropTypes.arrayOf(PropTypes.string).isRequired,
-  fieldAnswers: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default App;
