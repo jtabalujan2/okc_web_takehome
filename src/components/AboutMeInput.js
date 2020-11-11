5053e53607437dc02ee5c6cdee52dec5bd2a9580
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 13:46:37 -0800

    Added logic on when to submit field

--- src/components/AboutMeInput.js
+++ src/components/AboutMeInput.js
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 
 import { FIELDS } from '../constants';
 
-const AboutMeInput = ({ fieldName, submitField }) => {
+const AboutMeInput = ({ fieldName, fieldOrder, fieldAnswers, submitField,  }) => {
   const handleSubmit = (e) => {
+
     const payload = {
       id: fieldName,
-      answer: e.target.value
+      answer: e.target.value,
     }
 
-    submitField(payload)
-
+    //checks if the state is different; if it is, then submit it
+    if(fieldAnswers[fieldName] !== e.target.value && e.target.value !== "") {
+      submitField(payload)
+    }
   }
 
   return (
@@ -23,6 +26,7 @@ const AboutMeInput = ({ fieldName, submitField }) => {
 
 AboutMeInput.propTypes = {
   fieldName: PropTypes.string.isRequired,
+  fieldAnswers: PropTypes.object.isRequired,
   submitField: PropTypes.func.isRequired,
 };
 
