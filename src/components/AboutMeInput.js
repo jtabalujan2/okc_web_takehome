39a3acec95e75a63fd9921fbe2998af1d80e16e6
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 00:28:46 -0800

    Removing unused className

--- src/components/AboutMeInput.js
+++ src/components/AboutMeInput.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 import { FIELD_NAMES, COPY } from '../constants';
 
 const AboutMeInput = ({ fieldName }) => (
-  <div className="">
+  <div>
     <p>{COPY[FIELD_NAMES[fieldName]]}</p>
-    <input className="" />
+    <input />
   </div>
 );
 
