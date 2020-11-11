e357999a49d440ac8e771a08f9199ae34d2f9cab
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 09:47:54 -0800

    Finally used the right constant and the concept y'all were using LOL

--- src/components/AboutMeInput.js
+++ src/components/AboutMeInput.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { FIELD_NAMES, COPY } from '../constants';
+import { FIELDS } from '../constants';
 
 const AboutMeInput = ({ fieldName }) => (
   <div>
-    <p>{COPY[FIELD_NAMES[fieldName]]}</p>
+    <p>{FIELDS[fieldName]}</p>
     <input />
   </div>
 );
