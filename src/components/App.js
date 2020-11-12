dd74c59b8ee1f980f1bfce221a1d506f7628bf38
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 16:07:59 -0800

    Added logic to render edit portion

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import AboutMeEdit from "./AboutMeEdit"
 import AboutMeForm from "./AboutMeForm"
 
 const okcLogo = 'https://cdn.okccdn.com/media/img/hub/mediakit/okcupid_darkbg_2019.png';
 
-const App = ( ) => (
+const App = ({ isEdit }) => {
+
+  const Form = () => {
+    if(isEdit) {
+      return <AboutMeEdit />
+    }
+    else {
+      return <AboutMeForm />
+    }
+  }
+
+  return (
   <div className="flex flex-col justify-center items-center min-h-screen">
     <img src={okcLogo} alt="OkCupid Logo" />
     <div className="match-area">
-      <AboutMeForm />
-      <AboutMeEdit />
+      <Form />
     </div>
   </div>
-);
+)};
+
+App.propTypes = {
+  isEdit: PropTypes.bool.isRequired
+}
 
 
 export default App;
