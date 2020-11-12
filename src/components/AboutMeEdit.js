797d1747dcee0ffc2f5ed9c2d8d49e019b5864cd
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 16:35:46 -0800

    Created edit final page

--- src/components/AboutMeEdit.js
+++ src/components/AboutMeEdit.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 
-const AboutMeForm = () => (
-  <div className="flex">
+const AboutMeEdit = ({ essayText, submitReset, editEssay }) => {
+   const handleReset = (e) => {
+      e.preventDefault();
+      submitReset();
+   }
 
+   const handleEdit = (e) => {
+      e.preventDefault();
+      editEssay({essay: e.target.value})
+   }
+
+   return (
+  <div className="edit-field flex">
+      <input value={essayText} onChange={handleEdit}></input>
+      <button onClick={handleReset}>Start Over</button>
   </div>
-);
+)};
 
 
-export default AboutMeForm;
+export default AboutMeEdit;
