d56f065e16633ddb85bbd14c69196af478ed9a49
julian.tabalujan <julian.tabalujan@payforward.com>
2020-11-11 16:36:15 -0800

    Changed formatting to render

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import AboutMeEdit from "./AboutMeEdit"
+import AboutMeEdit from "../containers/AboutMeEdit"
 import AboutMeForm from "./AboutMeForm"
 
 const okcLogo = 'https://cdn.okccdn.com/media/img/hub/mediakit/okcupid_darkbg_2019.png';
 
 const App = ({ isEdit }) => {
 
-  const Form = () => {
-    if(isEdit) {
-      return <AboutMeEdit />
-    }
-    else {
-      return <AboutMeForm />
-    }
-  }
-
   return (
   <div className="flex flex-col justify-center items-center min-h-screen">
     <img src={okcLogo} alt="OkCupid Logo" />
     <div className="match-area">
-      <Form />
+      {isEdit ? <AboutMeEdit /> : <AboutMeForm />}
     </div>
   </div>
 )};
